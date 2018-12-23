fde25ee111de4504f96f184fe2ce2b370f8eb27c
Ryhon <ryhon0@gmail.com>
2018-12-23 19:49:22 +0100

    Updated the console application

--- src/ImageSplitter/Program.cs
+++ src/ImageSplitter/Program.cs
@@ -11,12 +11,35 @@ namespace ImageSplitter
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter the file path");
+            Console.WriteLine("Enter the image file path");
             string file = Console.ReadLine();
+
+            int? size = null;
+            while (size == null)
+            {
+                try
+                {
+                    Console.WriteLine("Enter the emote size");
+                    size = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Failed to parse the size");
+                }
+            }
+
+            Console.WriteLine("Enter the emote prefix");
+            string prefix = Console.ReadLine();
+
+            Console.WriteLine("Enter the emote surfix");
+            string surfix = Console.ReadLine();
+
             Console.WriteLine("Enter the output folder path");
             string output = Console.ReadLine();
-            Console.WriteLine("Generating emojis, please wait");
-            Splitter.Split(Image.FromFile(file), 128, output);
+
+            Console.WriteLine("Generating emojis, please wait...");
+            Splitter.Split(Image.FromFile(file), (int)size, output, prefix, surfix);
+
             Console.WriteLine("Done! Press any key to close");
             Console.ReadKey();
         }
